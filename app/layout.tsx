3d8228b8bca5151d990bc00b6eac4b75d742466d
programadorallsavfe <programador.allsavfe@gmail.com>
2025-10-24 15:49:21 -0500

    acsdac

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <SystemInitializer>
